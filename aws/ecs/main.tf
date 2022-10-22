d9314cb03a60051087dd7309263b37354734b79c
Yoav Klein <yoavklein25@gmail.com>
2022-10-23 00:10:48 +0300

    ECS Service

--- aws/ecs/main.tf
+++ aws/ecs/main.tf
@@ -64,18 +64,19 @@ EOF
 }
 
 
+
 resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attachment" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-resource "aws_ecs_task_definition" "counter-service" {
+resource "aws_ecs_task_definition" "counter_service" {
   family = "counter-service" # name
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn       = "arn:aws:iam::506189848464:role/ecsTaskExecutionRole"  # aws_iam_role.ecs_task_execution_role.arn
   container_definitions = jsonencode([
     {
       name      = "counter-service"
-      image     = "${aws_ecr_repository.counter_service.repository_url}:0.1"
+      image     = "${aws_ecr_repository.counter_service.repository_url}:latest"
       cpu       = 1
       memory    = 1024
       essential = true
@@ -88,4 +89,67 @@ resource "aws_ecs_task_definition" "counter-service" {
   network_mode = "awsvpc"
 }
 
+############
+# ECS Service
+#############
 
+# data sources to get VPC and subnets
+data "aws_vpc" "default" {
+    default = true
+}
+
+data "aws_subnets" "all" {
+    filter {
+        name = "vpc-id"
+        values = [data.aws_vpc.default.id]
+    }
+}
+
+resource "aws_security_group" "task_sg" {
+    name = "task_sg"
+    # vpc - defaults to the deafult vpc
+     
+    ingress {
+        description = "HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    
+    ingress {
+        description = "HTTPS"
+        from_port = 443
+        to_port = 443
+        protocol = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    } 
+
+}
+
+
+resource "aws_ecs_service" "counter_service" {
+  name            = "counter_service"
+  cluster         = aws_ecs_cluster.counter_service.id
+  task_definition = aws_ecs_task_definition.counter_service.arn
+  desired_count   = 1
+  launch_type = "FARGATE"
+#  load_balancer {
+#    target_group_arn = aws_lb_target_group.foo.arn
+#    container_name   = "mongo"
+#    container_port   = 8080
+#  }
+
+  network_configuration {
+    subnets = data.aws_subnets.all.ids
+    security_groups = [aws_security_group.task_sg.id]
+
+  }
+}
