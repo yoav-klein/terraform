0884ea1bd99b1090e994354797739b1f83b680b8
Yoav Klein <yoavklein25@gmail.com>
2023-01-19 12:53:08 +0000

    Improving VPC

--- aws/modules/vpc/main.tf
+++ aws/modules/vpc/main.tf
@@ -1,19 +1,41 @@
 
-data "aws_availability_zones" "available" {
-	state = "available"
+
+# - VPC
+# - the user specifies a list of "public_subnets": { "cidr", "az" }
+# - the user specifies a list of "private_subnets": {"cidr", "az"}
+#
+#
+
+
+data "aws_availability_zones" "this" {
+  state = "available"
 }
 
-resource "aws_vpc" "vpc" {
-	cidr_block = "10.0.0.0/16"
-	tags = {
-		Name = var.vpc_name
-	}
+resource "aws_vpc" "this" {
+  cidr_block = var.cidr
+  tags = {
+    Name = var.vpc_name
+  }
 }
 
+#####
+# Internet gateway created only when the user
+# defines public subnets
 
-resource "aws_subnet" "subnet" {
-	count = length(data.aws_availability_zones.available.names)
-	vpc_id = aws_vpc.vpc.id
-	availability_zone = data.aws_availability_zones.available.names[count.index]
-	cidr_block = "10.0.${count.index}.0/24"
+resource "aws_internet_gateway" "this" {
+    count = length(var.public_subnets) > 0 ? 1 : 0
+    vpc_id = aws_vpc.this.id
+    tags = {
+        Name = "${var.vpc_name}-igw"
+    }
+}
+
+resource "aws_subnet" "public_subnets" {
+  count = length(var.public_subnets)
+  vpc_id            = aws_vpc.this.id
+  availability_zone = var.public_subnets[count.index].az
+  cidr_block        = var.public_subnets[count.index].cidr
+  tags = {
+    Name = "${var.vpc_name}-pub-subent-${count.index}"
+  }
 }
--- aws/modules/vpc/varialbes.tf
+++ aws/modules/vpc/varialbes.tf
@@ -1,5 +1,28 @@
 
 variable "vpc_name" {
-	type = string
-	default = "my-vpc"
+  type    = string
+  default = "my-vpc"
+}
+
+variable "cidr" {
+    description = "The CIDR range for the VPC"
+    type = string
+}
+
+variable "public_subnets" {
+    description = "A list of public subnets to create in the VPC"
+    default = []
+    type = list(object({
+        cidr = string
+        az = string
+    }))
+}
+
+variable "private_subnets" {
+    description = "A list of privaate subnets to create in the VPC"
+    default = []
+    type = list(object({
+        cidr = string
+        az = string
+    }))
 }
