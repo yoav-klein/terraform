2d15d6baadb11296ec1bd07fb13bcf74e34ee8fe
Yoav Klein <yoavklein25@gmail.com>
2023-01-29 13:36:13 +0000

    Adding a configuration for testing Global Accelerator

---
+++ aws/configurations/for-global-accelerator/main.tf
@@ -0,0 +1,35 @@
+
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 4.50"
+        }
+    }
+}
+
+module "vpc" {
+    source = "../../modules/vpc"
+    name = "my_vpc"
+    cidr = "10.0.0.0/16"
+    public_subnets = [{ 
+        az = "us-east-1b"
+        cidr = "10.0.1.0/24"
+    }]
+    private_subnets = [{
+        az = "us-east-1a",
+        cidr = "10.0.2.0/24"
+    }]
+}
+
+module "pvt-instance" {
+    source = "../../modules/ec2"
+    instance_count = 3
+    
+    ami = "ami-00874d747dde814fa"
+    name = "pvt-instance"
+    pub_key_path = "${path.module}/aws.pub"
+    subnet_ids = module.vpc.private_subnet_ids
+    default_vpc = false
+    vpc_id = module.vpc.vpc_id
+}
