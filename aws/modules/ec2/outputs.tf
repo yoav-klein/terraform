be398188c42515369467e425cfd60f0a0b2fd18c
Yoav Klein <yoavklein25@gmail.com>
2023-02-17 08:02:12 +0000

    Modules::EC2:: adding public_dns output

--- aws/modules/ec2/outputs.tf
+++ aws/modules/ec2/outputs.tf
@@ -3,3 +3,8 @@ output "instance_ids" {
     description = "ID of the instance"
     value = aws_instance.this.*.id
 }
+
+output "public_dns" {
+    description = "Public DNS of the host"
+    value = aws_instance.this.*.public_dns
+}
