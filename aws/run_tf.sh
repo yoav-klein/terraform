1c8b068cd8db2c46d411638696283cae4b953ae7
Yoav Klein <yoavklein25@gmail.com>
2023-02-23 14:09:16 +0000

    run_tf.sh:: using aws credentials file

--- aws/run_tf.sh
+++ aws/run_tf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ] || [ -z "$AWS_REGION" ]; then
+if [ ! -f ~/.aws/credentials ] &&  ([ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ] || [ -z "$AWS_REGION" ]); then
     echo "export the AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY and AWS_REGION variables"
     exit 1
 fi
@@ -11,7 +11,7 @@ command="$@"
 
 [ -z "$command" ] && echo "Enter command" && exit 1
 
-docker run -it -v $script_dir:$script_dir \
+docker run -it -v ~/.aws:/root/.aws -v $script_dir:$script_dir \
     -e AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
     -e AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
     -e AWS_REGION="$AWS_REGION" \
