63619ce7bd56020db80aed4040028388ab1e08e3
Yoav Klein <yoavklein25@gmail.com>
2023-03-05 15:29:01 +0000

    aws::test::vpc:: formatting

--- aws/test/vpc/main.tf
+++ aws/test/vpc/main.tf
@@ -1,29 +1,29 @@
 
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 4.16"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
     }
+  }
 }
 
 provider "aws" {
 }
 module "vpc" {
-	source = "../../modules/vpc"
-	vpc_name = "my_test_vpc"
-    cidr = "10.0.0.0/16"
-    public_subnets = [{ 
-        az = "us-east-1b"
-        cidr = "10.0.1.0/24"
+  source   = "../../modules/vpc"
+  vpc_name = "my_test_vpc"
+  cidr     = "10.0.0.0/16"
+  public_subnets = [{
+    az   = "us-east-1b"
+    cidr = "10.0.1.0/24"
     },
     {
-        az = "us-east-1c"
-        cidr = "10.0.2.0/24"
-    }]
-    private_subnets = [{
-        az = "us-east-1a",
-        cidr = "10.0.3.0/24"
-    }]
+      az   = "us-east-1c"
+      cidr = "10.0.2.0/24"
+  }]
+  private_subnets = [{
+    az   = "us-east-1a",
+    cidr = "10.0.3.0/24"
+  }]
 }
