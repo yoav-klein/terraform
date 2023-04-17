4b500b3d6139904057c42f81f19954355704ca97
Yoav Klein <yoavklein25@gmail.com>
2023-04-17 07:46:41 +0000

    fmt

--- aws/test/vpc/main.tf
+++ aws/test/vpc/main.tf
@@ -11,9 +11,9 @@ terraform {
 provider "aws" {
 }
 module "vpc" {
-  source   = "../../modules/vpc"
-  name = "my_test_vpc"
-  cidr     = "10.0.0.0/16"
+  source = "../../modules/vpc"
+  name   = "my_test_vpc"
+  cidr   = "10.0.0.0/16"
   public_subnets = [{
     az   = "us-east-1b"
     cidr = "10.0.1.0/24"
