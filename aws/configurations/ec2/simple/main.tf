beeacef987bfb9f98d532d11d12e6c61a6669c60
Yoav Klein <yoavklein25@gmail.com>
2023-04-19 11:04:31 +0000

    aws::configurations::ec2::simple - adding map of AMIs

--- aws/configurations/ec2/simple/main.tf
+++ aws/configurations/ec2/simple/main.tf
@@ -11,10 +11,16 @@ terraform {
 
 provider "aws" {}
 
+locals {
+    amis = {
+        ubuntu = "ami-0f1bae6c3bedcc3b5"
+    }
+}
 
 module "ec2" {
     source = "../../../modules/ec2"
     name = "my-server"    
+    ami = local.amis["ubuntu"]
     pub_key_path="${path.module}/aws.pub"
     instance_count = 1
     default_vpc = true
