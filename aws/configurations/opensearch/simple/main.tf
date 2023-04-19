316911b7f1c334447abf4722687358f2cdd4419c
Yoav Klein <yoavklein25@gmail.com>
2023-04-19 11:17:01 +0000

    aws::configurations::opensearch::simple - minor thing

--- aws/configurations/opensearch/simple/main.tf
+++ aws/configurations/opensearch/simple/main.tf
@@ -10,6 +10,11 @@ terraform {
 
 provider "aws" {}
 
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
+
+
 resource "aws_opensearch_domain" "this" {
   domain_name    = "example"
   engine_version = "OpenSearch_2.3"
@@ -17,12 +22,36 @@ resource "aws_opensearch_domain" "this" {
   cluster_config {
     instance_type = "t3.medium.search"
   }
-  
+ 
+  encrypt_at_rest {
+    enabled = true
+  }
+
+  node_to_node_encryption {
+    enabled = true
+  }
+ 
   ebs_options {
     ebs_enabled = true
     volume_size = 15
   }
-    
+
+# this access policy is not allowed since it's too permissive
+/*
+  access_policies = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "es:*",
+      "Principal": {"AWS": "*"},
+      "Effect": "Allow",
+      "Resource": "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/example/*"
+    }
+  ]
+}
+POLICY
+*/  
  
   tags = {
     Domain = "TestDomain"
