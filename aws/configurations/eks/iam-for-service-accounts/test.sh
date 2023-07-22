41b456920d588f68bb0ec94d0312c03093a7dccc
Yoav Klein <yoavklein25@gmail.com>
2023-07-22 19:17:47 +0000

    aws::configuratiosn::eks::irsa

--- aws/configurations/eks/iam-for-service-accounts/test.sh
+++ aws/configurations/eks/iam-for-service-accounts/test.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 setup() {
-    shopt -s expand_aliases
-    source ../../../environment
-
-    bucket_name=$(tf output -raw s3_bucket_name)
+    bucket_name=$(terraform output -raw s3_bucket_name)
     account_id=$(aws sts get-caller-identity --query Account --output text)
 
     aws s3 cp content.txt s3://${bucket_name}/content.txt
 
-    sed "s/<account_id>/$account_id/" pod.yaml | kubectl apply -f -
+    sed "s/<account_id>/$account_id/" pod.yaml.template > pod.yaml
+    kubectl apply -f pod.yaml
 }
 
 test() {
