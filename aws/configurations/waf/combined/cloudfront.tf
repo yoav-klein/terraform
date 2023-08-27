3854bfab545d838c0a1c9ff71e0a1f1172260c90
Yoav Klein <yoavklein25@gmail.com>
2023-08-27 20:53:32 +0000

    update

--- aws/configurations/waf/combined/cloudfront.tf
+++ aws/configurations/waf/combined/cloudfront.tf
@@ -11,12 +11,31 @@ terraform {
 provider "aws" {}
 
 resource "aws_s3_bucket" "this" {
-  bucket = "mytestbucket-yoav"
+  bucket = "yoav-test-waf-combined"
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
+  bucket = aws_s3_bucket.this.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+  depends_on = [aws_s3_bucket_public_access_block.this]
 }
 
 resource "aws_s3_bucket_acl" "this" {
   bucket = aws_s3_bucket.this.id
   acl    = "public-read"
+
+  depends_on = [aws_s3_bucket_public_access_block.this]
 }
 
 
