09967d9df06f1356a541ac4cc9b4d1afb8050932
Yoav Klein <yoavklein25@gmail.com>
2024-03-12 07:11:48 +0000

    aws::configurations::ec2:: Simple server

--- aws/configurations/ec2/simple/main.tf
+++ aws/configurations/ec2/simple/main.tf
@@ -1,15 +1,12 @@
 
-
 terraform {
     required_providers {
         aws = {
             source = "hashicorp/aws"
-            version = "~> 4.16"
+            version = "~> 4.50"
         }
     }
 }
-
-provider "aws" {}
 
 locals {
     amis = {
@@ -17,17 +14,88 @@ locals {
     }
 }
 
-module "ec2" {
-    source = "../../../modules/ec2"
-    name = "my-server"    
-    ami = local.amis["ubuntu"]
-    pub_key_path="${path.module}/aws.pub"
-    instance_count = 1
-    default_vpc = true
-    
+
+module "vpc" {
+    source = "../../../modules/vpc"
+    name = "my_vpc"
+    cidr = "10.0.0.0/16"
+    public_subnets = [{ 
+        az = "us-east-1b"
+        cidr = "10.0.1.0/24"
+    }]
+    private_subnets = [{
+        az = "us-east-1a",
+        cidr = "10.0.2.0/24"
+    }]
 }
 
-output "public_dns" {
-    description = "public DNS"
-    value = module.ec2.public_dns
+
+resource "aws_security_group" "this" {
+  name = "nginx-server"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "tls_private_key" "this" {
+    algorithm = "RSA"
+}
+
+resource "local_file" "private_key" {
+    content = tls_private_key.this.private_key_pem
+    filename = "${path.root}/priv.key"
+    file_permissions = "0600"
+}
+
+resource "aws_key_pair" "this" {
+  key_name   = "nginx-keypair"
+  public_key = tls_private_key.this.public_key_openssh
+}
+
+resource "aws_instance" "this" {
+  ami                  = local.amis["ubuntu"]
+  key_name             = aws_key_pair.this.key_name
+  instance_type        = "t2.small"
+  vpc_security_group_ids = [ aws_security_group.this.id ]
+  subnet_id = module.vpc.public_subnet_ids[0]
+  
+  tags = {
+    Name = "Nginx"
+  }
+
+}
+
+output "server_domain" {
+    description = "Domain name"
+    value = aws_instance.this.public_dns
 }
