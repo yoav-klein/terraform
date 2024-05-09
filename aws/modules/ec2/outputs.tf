fb09d1955ee4878d0753c0e914317c8de73fd684
Yoav Klein <yoavklein25@gmail.com>
2024-05-09 07:45:00 +0000

    minor changes

--- aws/modules/ec2/outputs.tf
+++ aws/modules/ec2/outputs.tf
@@ -1,4 +1,9 @@
 
+output "key_pair_name" {
+   description = "Key pair name"
+   value = aws_key_pair.this.key_name
+}
+
 output "instance_ids" {
     description = "ID of the instance"
     value = aws_instance.this.*.id
