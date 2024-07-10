0bf544ec3bf0a8a0b12a02bc57756e145c773b6c
Yoav Klein <yoavklein25@gmail.com>
2024-07-10 05:07:41 +0000

    EKS and Opensearch

--- aws/configurations/eks-and-opensearch/configure_kubeconfig.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-aws eks update-kubeconfig --region us-east-1 --name eks-opensearch
--- aws/configurations/eks-and-opensearch/get-password.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-windows_id=$(terraform output -raw windows_instance_id)
-
-aws ec2 get-password-data --instance-id $windows_id --priv-launch-key private.key > password
-
