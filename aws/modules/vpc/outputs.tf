fcfa7dcdbec47a24ae18272a9d69179fdc5fc0a6
Yoav Klein <yoavklein25@gmail.com>
2024-08-05 22:21:54 +0000

    VPC module update

--- aws/modules/vpc/outputs.tf
+++ aws/modules/vpc/outputs.tf
@@ -19,3 +19,13 @@ output "default_route_table_id" {
     description = "Default route table ID"
     value = aws_vpc.this.default_route_table_id
 }
+
+output "public_route_table" {
+    description = "Public route table"
+    value = aws_route_table.public[0].id
+}
+
+output "private_route_table" {
+    description = "Private route table"
+    value = aws_route_table.private[0].id
+}
