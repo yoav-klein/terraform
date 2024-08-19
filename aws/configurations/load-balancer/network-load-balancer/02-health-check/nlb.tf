95374bc934936fb742070f0fa3bb1236ffa2103f
Yoav Klein <yoavklein25@gmail.com>
2024-08-19 10:39:12 +0000

    aws::configurations::load-balancer:: Health Checks

--- aws/configurations/load-balancer/network-load-balancer/02-health-check/nlb.tf
+++ aws/configurations/load-balancer/network-load-balancer/02-health-check/nlb.tf
@@ -2,23 +2,23 @@
 ### Security group for the Load Balancer
 
 resource "aws_security_group" "load_balancer" {
-    name = "els-security-group"
-    vpc_id = module.vpc.vpc_id
-    ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "TCP"
-        description = "Echo server"
-        cidr_blocks = ["0.0.0.0/0"]        
-    }
-   
-    egress {
-        from_port        = 0
-        to_port          = 0
-        protocol         = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = ["::/0"]
-    }
+  name   = "els-security-group"
+  vpc_id = module.vpc.vpc_id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "TCP"
+    description = "Echo server"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
 }
 
@@ -41,7 +41,7 @@ resource "aws_lb_listener" "front_end" {
   load_balancer_arn = aws_lb.this.arn
   port              = "80"
   protocol          = "TCP"
- 
+
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.this.arn
@@ -49,17 +49,17 @@ resource "aws_lb_listener" "front_end" {
 }
 
 resource "aws_lb_target_group" "this" {
-  name     = "my-tg"
-  port     = 5000
-  protocol = "TCP"
-  vpc_id   = module.vpc.vpc_id
+  name        = "my-tg"
+  port        = 5000
+  protocol    = "TCP"
+  vpc_id      = module.vpc.vpc_id
   target_type = "instance"
 
-   health_check {
-     path = "/health"
-     port = 8090
-     protocol = "HTTP"
-   }
+  health_check {
+    path     = "/health"
+    port     = 8090
+    protocol = "HTTP"
+  }
 }
 
 resource "aws_lb_target_group_attachment" "this" {
@@ -71,6 +71,6 @@ resource "aws_lb_target_group_attachment" "this" {
 }
 
 output "nlb-domain-name" {
-    description = "Domain name of the Network Load Balancer"
-    value = aws_lb.this.dns_name
+  description = "Domain name of the Network Load Balancer"
+  value       = aws_lb.this.dns_name
 }
--- aws/configurations/load-balancer/network-load-balancer/02-health-check/vpc.tf
+++ aws/configurations/load-balancer/network-load-balancer/02-health-check/vpc.tf
@@ -5,26 +5,26 @@
 
 # VPC with 2 private subnets, and 2 public subnets
 module "vpc" {
-  source   = "../../../../modules/vpc"
-  name = "my-vpc"
-  cidr     = "10.0.0.0/16"
+  source = "../../../../modules/vpc"
+  name   = "my-vpc"
+  cidr   = "10.0.0.0/16"
   private_subnets = [{
-        az   = "us-east-1b"
-        cidr = "10.0.1.0/24"
+    az   = "us-east-1b"
+    cidr = "10.0.1.0/24"
     },
     {
       az   = "us-east-1c"
       cidr = "10.0.2.0/24"
-    }]
-    public_subnets = [{
-        az   = "us-east-1b",
-        cidr = "10.0.3.0/24"
+  }]
+  public_subnets = [{
+    az   = "us-east-1b",
+    cidr = "10.0.3.0/24"
     },
     {
-        az = "us-east-1c",
-        cidr = "10.0.4.0/24"
+      az   = "us-east-1c",
+      cidr = "10.0.4.0/24"
     }
-    ]
+  ]
 }
 
 # EIP for the NAT Gateway
