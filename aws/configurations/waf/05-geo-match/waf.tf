ac328669599158eea2f9abe13cc20c228e77fb59
Yoav Klein <yoavklein25@gmail.com>
2024-08-27 06:31:48 +0000

    waf::geo-match:: Add logging

--- aws/configurations/waf/05-geo-match/waf.tf
+++ aws/configurations/waf/05-geo-match/waf.tf
@@ -41,3 +41,14 @@ resource "aws_wafv2_web_acl" "this" {
  }
  
 
+resource "aws_cloudwatch_log_group" "this" {
+    name = "aws-waf-logs-test"
+}
+
+resource "aws_wafv2_web_acl_logging_configuration" "this" {
+    log_destination_configs = [aws_cloudwatch_log_group.this.arn]
+    resource_arn = aws_wafv2_web_acl.this.arn
+}
+
+
+
