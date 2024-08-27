4bff9a23a71dad945bdbd50de1eba4e46bbedde8
Yoav Klein <yoavklein25@gmail.com>
2024-08-27 06:04:51 +0000

    waf

---
+++ aws/configurations/waf/05-geo-match/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+setup() {
+    bucket_name=$(tf output -raw bucket_name)
+    aws s3 cp index.html "s3://${bucket_name}/index.html" --acl="public-read"
+}
+
+test() {
+    
+    cloudfront_url=$(terraform output -raw cloudfront_url)
+    curl ${cloudfront_url}/index.html
+}
